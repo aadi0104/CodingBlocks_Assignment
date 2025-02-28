82d4e44f487cdea0b0f9705a0c581e01e62320a5
Aadil Ahmad <aadill0104@gmail.com>
2025-02-28 14:23:39 +0530

    Highest frequency (Hashing)

---
+++ CodingBlocks_Assignments/src/Assignment9/Highest_Frequency.java
@@ -0,0 +1,30 @@
+package Assignment9;
+
+import java.util.*;
+
+public class Highest_Frequency {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			int data = sc.nextInt();
+			if (map.containsKey(data)) {
+				map.put(data, map.get(data) + 1);
+			} else {
+				map.put(data, 1);
+			}
+		}
+		int maxkey = 0;
+		int maxdata = 0;
+		for (int key : map.keySet()) {
+			if (map.get(key) > maxdata) {
+				maxkey = key;
+				maxdata = map.get(key);
+			}
+		}
+		System.out.println(maxkey);
+	}
+
+}
